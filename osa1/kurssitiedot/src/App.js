ad17444545e84e571ac234234a1dc6cc4645e814
Eetu Turtiainen <eetu.turtiainen@icloud.com>
2023-07-11 18:20:39 +0300

    Tein Osa 2 tehtäviä vahingossa Osa 1 kansioon.

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,56 +1,76 @@
-import Course from './components/Course'
-
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Course courses={courses}/>
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
+}
+
+const Header = (props) => {
+  console.log(props)
+  return (
+
+    <>
+      <h1>{props.course.name}</h1>
+    </>
+  )
+
+}
+
+const Content = (props) => {
+
+  return (
+
+    <>
+    <Part part={props.course.parts[0]}/>
+    <Part part={props.course.parts[1]}/>
+    <Part part={props.course.parts[2]}/>
+
+    </>
+  )
+
+}
+
+const Total = (props) => {
+
+  return (
+
+    <>
+    <p>Number of exercises {props.course.parts[0].exercises+ props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+    </>
+  )
+
+}
+
+const Part = (props) => {
+  console.log(props)
+  return (
+    
+    <>
+    <p>{props.part.name} {props.part.exercises}</p>
+    </>
+  )
+
 }
 
 
---
+++ osa2/kurssitiedot/README.md
@@ -0,0 +1,1 @@
+# Answers to the question 1.1 in Full Stack Open Course
