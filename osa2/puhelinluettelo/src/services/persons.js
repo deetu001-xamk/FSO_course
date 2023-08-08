8dfb65e3c4231da58fb5ce0586d4594332101ab5
Eetu Turtiainen <eetu.turtiainen@icloud.com>
2023-08-08 12:55:31 +0300

    Delete API fixed

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'https://puhelinluettelo-sovellus.onrender.com/api/persons'
 
 const getAll = () => {
     const req = axios.get(baseUrl)
