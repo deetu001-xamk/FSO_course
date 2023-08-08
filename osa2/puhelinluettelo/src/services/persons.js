1b5139a90b8c34a2cb4d3cea747515c908dae1c5
Eetu Turtiainen <eetu.turtiainen@icloud.com>
2023-08-08 16:28:25 +0300

    Build

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'https://puhelinluettelo-sovellus.onrender.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const req = axios.get(baseUrl)
